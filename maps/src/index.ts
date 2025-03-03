9db2e1288b723376f1303a8d7e81d58591c5de15
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-03 11:26:06 -0700

    feat: refactor map initialization with CustomMap class

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -3,10 +3,7 @@
 // import { User } from './User';
 // import { Company } from './Company';
 
-new google.maps.Map(document.getElementById('map') as HTMLDivElement, {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-});
+import { CustomMap } from "./CustomMap";
+
+const map = new CustomMap('map');
+
