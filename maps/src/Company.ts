910d40f9db059f99b40e01964c6fda2b1fa1e313
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-03 11:14:15 -0700

    feat: integrate Google Maps with initial map rendering

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,19 +1,19 @@
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 
 export class Company {
-    name: string;
-    catchPhrase: string;
-    location: {
-        lat: number;
-        lng: number;
-    }
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
 
-    constructor() {
-        this.name = faker.company.companySuffix();
-        this.catchPhrase = faker.company.catchPhrase();
-        this.location = {
-            lat: parseFloat(faker.address.latitude()),
-            lng: parseFloat(faker.address.latitude())
-        }
-    }
+  constructor() {
+    this.companyName = faker.company.name();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,19 +1,17 @@
 import { faker } from "@faker-js/faker";
 
-class User {
-    name: string;
-    location: {
-        lat: number;
-        lng: number;
-    }
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
 
-    constructor() {
-        this.name = faker.name.firstName();
-        this.location = {
-            lat: parseFloat(faker.address.latitude()),
-            lng: parseFloat(faker.address.longitude())
-        }
-    }
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
-
-export { User };
