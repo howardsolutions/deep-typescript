3f8775e293498eb8976e7d95257f2913b0e4b367
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-03 12:04:26 -0700

    feat: add marker content method to User and Company classes

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -6,7 +6,8 @@ interface Mappable {
     location: {
         lat: number,
         lng: number,
-    }
+    };
+    markerContent(): string;
 }
 
 export class CustomMap {
@@ -33,7 +34,7 @@ export class CustomMap {
 
         marker.addListener("click", () => {
             const infoWindow = new google.maps.InfoWindow({
-                content: "Hi there"
+                content: mappable.markerContent()
             })
             infoWindow.open(this.googleMap, marker)
         })
