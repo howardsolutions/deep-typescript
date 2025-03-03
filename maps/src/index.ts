b96effc8fa8d6ac496035629ac9810ba799c15a9
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-03 11:57:29 -0700

    feat: add markers for User and Company on CustomMap. Using dependency inversion to create scallable type solution - Mappable for function addMarker

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,13 @@
 /// <reference types="@types/google.maps" />
 
-// import { User } from './User';
-// import { Company } from './Company';
+import { User } from './User';
+import { Company } from './Company';
 
 import { CustomMap } from "./CustomMap";
 
-const map = new CustomMap('map');
+const customMap = new CustomMap('map');
+const user = new User();
+const company = new Company();
+
+customMap.addMarker(user);
+customMap.addMarker(company);
