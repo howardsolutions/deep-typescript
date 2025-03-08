f2fd368afd41f6007415e08c686838c4f4d28ec4
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-08 14:22:26 -0700

    refactor: seperate the sorting and number collection logic

--- 2. sort/src/index.ts
+++ 2. sort/src/index.ts
@@ -1,27 +1,7 @@
-class Sorter {
-  constructor(public collection: number[] | string) { }
+import { NumberCollection } from "./NumberCollections";
+import { Sorter } from "./sorter";
 
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-
-        if (typeof this.collection === 'string') {
-          // this.collection.charAt()
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const numberOfCollection = new NumberCollection([10, 3, -5, 0])
+const sorter = new Sorter(numberOfCollection);
 sorter.sort();
 console.log(sorter.collection);
