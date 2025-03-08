383eefecdebbf2b8c95d5121e3ed06386a94ce01
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-08 14:54:32 -0700

    refactor: introduce Sortable interface for generic sorting

--- 2. sort/src/index.ts
+++ 2. sort/src/index.ts
@@ -4,4 +4,4 @@ import { Sorter } from "./sorter";
 const numberOfCollection = new NumberCollection([10, 3, -5, 0])
 const sorter = new Sorter(numberOfCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numberOfCollection.data);
