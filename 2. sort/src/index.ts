f3460eda43084a152b1a49f3faf0bdecbefef225
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-09 12:03:57 -0600

    feat: add LinkedList implementation for sorting

--- 2. sort/src/index.ts
+++ 2. sort/src/index.ts
@@ -1,6 +1,8 @@
 // import { NumberCollection } from "./NumberCollections";
-import { CharacterCollections } from "./CharacterCollections";
+// import { CharacterCollections } from "./CharacterCollections";
+import { LinkedList } from "./LinkedList";
 import { Sorter } from "./sorter";
+
 
 // const numberOfCollection = new NumberCollection([10, 3, -5, 0])
 // const sorter = new Sorter(numberOfCollection);
@@ -8,7 +10,18 @@ import { Sorter } from "./sorter";
 // console.log(numberOfCollection.data);
 
 
-const characterCollection = new CharacterCollections('Xaayb')
-const sorter = new Sorter(characterCollection);
-sorter.sort();
-console.log(characterCollection.data);
+// const characterCollection = new CharacterCollections('Xaayb')
+// const sorter = new Sorter(characterCollection);
+// sorter.sort();
+// console.log(characterCollection.data);
+
+const linkedlist = new LinkedList();
+linkedlist.add(500)
+linkedlist.add(-10)
+linkedlist.add(-3)
+linkedlist.add(4)
+
+
+const sorted = new Sorter(linkedlist)
+sorted.sort();
+linkedlist.print()
