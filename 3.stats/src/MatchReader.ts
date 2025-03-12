9176ca7007a0e87fdb31d1f1d72f2d555fe09636
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-12 10:49:36 -0600

    feat: Solution with interface remove coupling classes

--- 3.stats/src/MatchReader.ts
+++ 3.stats/src/MatchReader.ts
@@ -1,5 +1,5 @@
-import { dateStringToDate } from "./utils";
 import { MatchResult } from "./enums";
+import { dateStringToDate } from "./utils";
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
@@ -8,20 +8,23 @@ interface DataReader {
     data: string[][];
 }
 
-class MatchReader<T> {
+export class MatchReader {
+    matches: MatchData[] = []
+
     constructor(public reader: DataReader) { }
 
-    load() { }
-
-    // .map((row: string[]): string => {
-    //     return [
-    //         dateStringToDate(row[0]),
-    //         row[1],
-    //         row[2],
-    //         parseInt(row[3]),
-    //         parseInt(row[4]),
-    //         row[5] as MatchResult,
-    //         row[6]
-    //     ]
-    // })
+    load(): void {
+        this.reader.read();
+        this.matches = this.reader.data.map((row: string[]): MatchData => {
+            return [
+                dateStringToDate(row[0]),
+                row[1],
+                row[2],
+                parseInt(row[3]),
+                parseInt(row[4]),
+                row[5] as MatchResult,
+                row[6]
+            ]
+        })
+    }
 }
