bb242f944ed4cbbc18db20ec0689d14c043a70db
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-15 11:53:37 -0600

    feat: enhance User class with trigger method for event handling

--- 4- web-framework/src/models/User.ts
+++ 4- web-framework/src/models/User.ts
@@ -3,7 +3,7 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => {}
+type Callback = () => void;
 
 export class User {
   events: { [key: string]: Callback[] } = {}
@@ -20,6 +20,20 @@ export class User {
   }
 
   on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
 
+    handlers.push(callback)
+
+    this.events[eventName] = [...handlers];
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length == 0) {
+      return
+    }
+
+    handlers.forEach(callback => callback())
   }
 }
