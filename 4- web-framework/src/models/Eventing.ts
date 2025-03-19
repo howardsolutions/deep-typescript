96d535c164067da9126bbbb3c22b0c78e6454232
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-19 13:28:34 -0600

    feat: implement passthrough methods

--- 4- web-framework/src/models/Eventing.ts
+++ 4- web-framework/src/models/Eventing.ts
@@ -3,7 +3,6 @@ type Callback = () => void;
 export class Eventing {
     events: { [key: string]: Callback[] } = {};
 
-
     on(eventName: string, callback: Callback): void {
         const handlers = this.events[eventName] || [];
         handlers.push(callback);
