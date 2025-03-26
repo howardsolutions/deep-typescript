c810daef799b831197be0df7c1a7f4009e2a0ac4
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-26 14:03:49 -0600

    feat: add login status route to display user authentication state

--- ts-express/src/routes/loginRoutes.ts
+++ ts-express/src/routes/loginRoutes.ts
@@ -34,4 +34,22 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
   }
 });
 
+router.get("/", (req: Request, res: Response) => {
+  if (req.session?.loggedIn) {
+    res.send(`
+        <div>
+          <div>You're logged in</div>
+          <a href="/logout">Logout</a>
+        </div>
+      `)
+  } else {
+    res.send(`
+      <div>
+        <div>You're NOT logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `)
+  }
+})
+
 export { router };
